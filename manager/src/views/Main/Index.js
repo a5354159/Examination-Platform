cdefc6da182cac94f1c4e6c59fa39ae145690da3
a5354159 <390354259@qq.com>
2019-06-19 21:28:27 +0800

    添加国际化

--- manager/src/views/Main/Index.js
+++ manager/src/views/Main/Index.js
@@ -23,17 +23,14 @@ function IndexPage(props) {
   return (
     <Layout className="container">
       <Header>
+        {/* <span
+          className="lang"
+          onClick={() => props.changeLocal(props.locale === "zh" ? "en" : "zh")}
+        >
+          {props.locale === "zh" ? "英文" : "中文"}
+        </span> */}
         <div className="header_box">
           <div>
-            <span>
-              {/* <button
-                onClick={() =>
-                  props.changeLocal(props.locale === "zh" ? "en" : "zh")
-                }
-              >
-                {props.locale === "zh" ? "中文" : "英文"}
-              </button> */}
-            </span>
             <img
               src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551624718911&di=4a7004f8d71bd8da84d4eadf1b59e689&imgtype=0&src=http%3A%2F%2Fimg105.job1001.com%2Fupload%2Falbum%2F2014-10-15%2F1413365052_95IE3msH.jpg"
               alt=""

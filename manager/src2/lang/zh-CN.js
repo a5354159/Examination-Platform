f7d1ecf9cbc1a2767fda7d0629fa136ec03a8c89
wangshiyu <1102404281@qq.com>
2019-06-23 19:16:57 +0800

    特批班级

--- manager/src2/lang/zh-CN.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "router.questions": "试题管理",
-  "router.questions.add": "添加试题",
-  "router.questions.type": "试题分类",
-  "router.questions.view": "查看试题"
-};
--- manager/src2/services/index.js
+++
@@ -1,5 +0,0 @@
-export * from './user.js';
-export * from './questions.js';
-export * from './ClssMang.js'
-export * from './MarkingManage.js'
-export * from './addexam.js';
